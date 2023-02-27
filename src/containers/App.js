0919423e75a565a2d75dbf5b542e85828e495600
Maksym Pecherytsia <findimrk@gmail.com>
2023-02-27 09:43:22 +0200

    add media query

--- src/containers/App.js
+++ src/containers/App.js
@@ -45,7 +45,7 @@ class App extends Component {
 		} else {
 			return (
 				<div className="tc">
-					<h1 className="f1">RoboFriends</h1>
+					<h1 className="f1 title">RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange} />
 					<Scroll>
 						<ErrorBoundry>
